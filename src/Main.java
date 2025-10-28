3b86edadf2cf7159cfdffd2bf9e1fa7641052c56
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-10-28 11:28:32 +0300

    add: introducing the string class and its methods and know more about the immutable string in java

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,15 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        // bad practice to declare multiple variables at the same line
-        // int age = 30, temperature = 32;
-        byte age = 30;
-        //int viewsCount = 10_333_222_222; // will throw an error (missing L,l casting)
-        long viewsCount = 10_333_222_222L;
-        double price = 19.22;
-        float degree = 87.88F; // f,F is mandatory here
-        // numbers with decimal points are double by default
-        // numbers with no decimal points are integer by default
-        char grade = 'A'; // must be single character and must be in '' single quote
-        boolean isEligible = false;
-        Date now = new Date();
-        System.out.println(now);
+        String message = "Hello World" + "!!";
 
+        // it creates a new string and not mutates the original message variable
+        // replace is a pure fn
+        System.out.println(message.replace("!", "&"));
+        System.out.println("the real message kept unchanged: " +  message);
+        System.out.println(message.endsWith("sky"));
+        System.out.println(message.indexOf("now"));
+        // the original string is immutable in java
 
-        Point point1 = new Point(1,2);
-        Point point2 = point1;
-
-        point1.x = 10;
-
-        System.out.println(point2);
     }
 }
