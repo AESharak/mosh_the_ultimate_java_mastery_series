e60de804be30fb6110662805d8db4a0876794a16
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-10-30 13:45:44 +0300

    introducing the final keyword and declaring constant variables

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,18 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-      // multiple dimensions array
-        int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6} };
-        // int [][] numbers = int [1][2];
+      // if we want to declare constants in our app
+        //float PI = 3.14f;
+        // with this I could do this
+        //PI = 2.4f;
+        // this is not correct as I want the PI variable to be constant
+        // so how to do this in java
 
+        final float PI = 3.14159265F;
+        //PI = 2134F; // EXCEPTION cannot assign a value to final variable PI
 
-        System.out.println(Arrays.deepToString(numbers));
+        System.out.println(PI);
+
 
 
 
