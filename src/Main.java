6135d19e53d85b659e38ca310fc80cda5c3be2aa
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-10-30 13:32:18 +0300

    introducing the multi deminsion arrays

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,14 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        // the old syntax
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        //numbers[10] = 3; Exception out of bounds
+      // multiple dimensions array
+        int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6} };
+        // int [][] numbers = int [1][2];
 
-        System.out.println(numbers); // a reference to the array [I@65ab7765
-        // to print the real values in a string representation you need to do this
 
-        System.out.println(Arrays.toString(numbers)); // [1, 2, 0, 0, 0]
+        System.out.println(Arrays.deepToString(numbers));
 
-        // arrays are with fixed size in java, fixed size once created never changed same datatype
 
-        int[] newNumbers = { 2, 3, 4, 1, 5};
-        //newNumbers[5] = 2; // Exception out of bounds
-
-        Arrays.sort(newNumbers); // impure fn, it mutates the original array
-
-        System.out.println(Arrays.toString(newNumbers));
 
 
 
