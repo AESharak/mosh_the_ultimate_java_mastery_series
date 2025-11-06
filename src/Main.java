3194d63e978e0274afdf485bc2498ef542256f29
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-11-06 15:40:16 +0200

    Formatting numbers in java using the NumberFormat class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
-        double randomNumber = Math.random();
-        System.out.println(randomNumber);
+        // NumberFormat is abstract class. ie, we can not instantiate it
+        // we can access some of its utilities which is methods or abstract methods
+        // these methods are related to the class itself so it's a static method
+        String result = NumberFormat.getCurrencyInstance().format(34243243.23);
+        String percentage = NumberFormat.getPercentInstance().format(.23);
 
-        int randomNumberBetweenZeroAndHundred = (int)(Math.random() * 100);
-        System.out.println(randomNumberBetweenZeroAndHundred);
+        System.out.println(result);
+        System.out.println(percentage);
     }
 }
