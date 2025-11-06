bcbb56f0b8e29fa8fdf3c0a5fe5d80c9005dfe84
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-11-06 15:19:45 +0200

    intoducing Math class and its useful methods

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,10 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        // casting (Implicit/Explicit)
-        short x = 2;
-        int y = x + 1; // Implicit casting
-        // byte > short > int > long > float > double
-        // both implicit and explicit casting must happen to compatible types
-        // like numbers as above the casting happens seamlessly
-        System.out.println(y);
+        double randomNumber = Math.random();
+        System.out.println(randomNumber);
 
-        double a = 1.2;
-        //int b = a + 2; // compilation error
-        int b = (int)a + 2;
-        double c = a + 2;
-        System.out.println(b);
-        System.out.println(c);
-
-        String p = "1.1";
-        //int d = (int)p + 2; // error
-        double d = Double.parseDouble(p) + 2;
-
-        // so in Java the casting between different types
-        //especially between string and different numbers
-        // we have to use Class.parseClass method
-        // this method if it can't parse the value it throws an exception
-
+        int randomNumberBetweenZeroAndHundred = (int)(Math.random() * 100);
+        System.out.println(randomNumberBetweenZeroAndHundred);
     }
 }
