bd9454637c87a7d2b0bcdbf17c7ea265dbb3617d
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-11-06 14:07:42 +0200

    introducing numbers and operations in java and casting

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,16 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-      // if we want to declare constants in our app
-        //float PI = 3.14f;
-        // with this I could do this
-        //PI = 2.4f;
-        // this is not correct as I want the PI variable to be constant
-        // so how to do this in java
+        //int resultingNumber = 10 / 3;
+        //System.out.println(resultingNumber); // print a  whole number
 
-        final float PI = 3.14159265F;
-        //PI = 2134F; // EXCEPTION cannot assign a value to final variable PI
+        // If you want to print the real resulting value
 
-        System.out.println(PI);
+        double resultingNumber = (double)10 / (double)3; // this called casting
+
+
+        System.out.println(resultingNumber);
+
 
 
 
