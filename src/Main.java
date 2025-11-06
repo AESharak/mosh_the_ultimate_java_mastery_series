fe15f8d52b239b994ab5bf5f32980f587aa086f9
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-11-06 14:29:38 +0200

    introducing casting and also the casting between different types especially between strings and other numeric types and the cases of this operation

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,29 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        //int resultingNumber = 10 / 3;
-        //System.out.println(resultingNumber); // print a  whole number
+        // casting (Implicit/Explicit)
+        short x = 2;
+        int y = x + 1; // Implicit casting
+        // byte > short > int > long > float > double
+        // both implicit and explicit casting must happen to compatible types
+        // like numbers as above the casting happens seamlessly
+        System.out.println(y);
 
-        // If you want to print the real resulting value
+        double a = 1.2;
+        //int b = a + 2; // compilation error
+        int b = (int)a + 2;
+        double c = a + 2;
+        System.out.println(b);
+        System.out.println(c);
 
-        double resultingNumber = (double)10 / (double)3; // this called casting
+        String p = "1.1";
+        //int d = (int)p + 2; // error
+        double d = Double.parseDouble(p) + 2;
 
-
-        System.out.println(resultingNumber);
-
-
-
-
-
-
+        // so in Java the casting between different types
+        //especially between string and different numbers
+        // we have to use Class.parseClass method
+        // this method if it can't parse the value it throws an exception
 
     }
 }
