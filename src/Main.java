771e7940cc2e336a8d5207940daefd582f375d00
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-11-06 15:52:40 +0200

    taking inputs from the user

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,14 @@ import java.awt.*;
 import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new  Scanner(System.in);
 
-        // NumberFormat is abstract class. ie, we can not instantiate it
-        // we can access some of its utilities which is methods or abstract methods
-        // these methods are related to the class itself so it's a static method
-        String result = NumberFormat.getCurrencyInstance().format(34243243.23);
-        String percentage = NumberFormat.getPercentInstance().format(.23);
+        int inputtedNo =  scanner.nextInt();
 
-        System.out.println(result);
-        System.out.println(percentage);
+        System.out.println(inputtedNo * 100);
     }
 }
