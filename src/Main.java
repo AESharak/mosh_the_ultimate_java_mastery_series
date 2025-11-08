fa9b249bf3efd0bed6d5f008a4337b02e97c41a3
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-11-08 13:20:57 +0200

    introducing the ternary operator

--- src/Main.java
+++ src/Main.java
@@ -8,35 +8,11 @@ public class Main {
     public static void main(String[] args) {
         int income = 120_000;
 
-        // will throw an error
-        // as You have to use {} to declare a variable
-//        if (income > 100_000)
-//            boolean hasHighIncome = true;
+        String className = income > 100_000 ? "First" : "Economy";
+
+        System.out.println(className);
 
 
-        // the problem in this case is that hasHighIncome is scoped to this if block
-        // and I can't access it outside this if block
-        if (income > 100_000) {
-            boolean hasHighIncome = true;
-        }
-
-        // another not good but still bad approach
-//        boolean hasHighIncome;
-//        if (income > 100_000) {
-//            hasHighIncome = true;
-//        } else {
-//            hasHighIncome = false;
-//        }
-
-        // better approach but still not that good
-//        boolean hasHighIncome = false;
-//        if (income > 100_000) {
-//            hasHighIncome = true;
-//        }
-
-        // the best approach right here
-        boolean hasHighIncome = (income > 100_000);
-        System.out.println(hasHighIncome);
 
     }
 }
